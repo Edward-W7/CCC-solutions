a51877e916c4ec22de5542a0996ea980879798a7
Edward Wang <115112114+Edward-W7@users.noreply.github.com>
2022-12-04 21:02:40 -0500

    Create CCC2011s4.py

---
+++ CCC2011s4.py
@@ -0,0 +1,58 @@
+#this solution is correct on the official data, but it might not be fully complete
+import sys
+import math
+import bisect
+from sys import stdin, stdout
+from math import gcd, floor, sqrt, log, inf
+
+sys.setrecursionlimit(690000000)
+
+
+# actual code
+
+blood = [int(i) for i in stdin.readline().split()]
+patient = [int(i) for i in stdin.readline().split()]
+ans = 0
+
+
+def allo(a, b):
+    global ans
+    global blood
+    global patient
+    alo = min(blood[a], patient[b])
+    blood[a] -= alo
+    patient[b] -= alo
+
+    ans += alo
+
+
+allo(0, 0)
+
+allo(1, 1)
+
+allo(2, 2)
+
+allo(4, 4)
+
+allo(1, 3)
+allo(2, 3)
+allo(3, 3)
+
+allo(1, 5)
+allo(4, 5)
+allo(5, 5)
+
+allo(2, 6)
+allo(4, 6)
+allo(6, 6)
+
+allo(0, 1)
+allo(0, 2)
+allo(0, 3)
+allo(0, 4)
+allo(0, 5)
+allo(0, 6)
+
+ans += min(sum(blood), patient[7])
+
+print(ans)
